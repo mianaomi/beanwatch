f988a15f174e240aa57c3e487720a3422657b48a
miac-umdacc <miacohen@terpmail.umd.edu>
2025-01-13 15:47:38 -0500

    Setup/Hello World Part 2!

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ UMD Coffee Club's Very Own Bespoke Bookkeeping/Inventory Software!
 Welcome to our page! 
 
 This is a 2025 Project By:
-Mia Cohen
-Yusuf Hasan
-Michael Wang
-Lauren Rabe
+Mia Cohen,
+Yusuf Hasan,
+Michael Wang,
+Lauren Rabe,
 Ian Tran
--- bookkeeper/views.py
+++ bookkeeper/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    html_content = """
+    <html>
+        <head>
+            <title>Home Page</title>
+        </head>
+        <body>
+            <h1 style="color: brown; font-family: Helvetica;">Hello, Coffee@UMD!</h1>
+        </body>
+    </html>
+    """
+    return HttpResponse(html_content)
